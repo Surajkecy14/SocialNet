4343e7aee8b92b174c7f7c341ccc409253cd8b8a
Surajkecy14 <147683761+Surajkecy14@users.noreply.github.com>
2025-07-16 16:26:42 +0545

    trying fix the problem

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: ['https://social-net-taupe.vercel.app', 'http://localhost:3000'],
   credentials: true,
 }));
 
